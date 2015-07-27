9ac37bd78b35a5f6bd394cafbdb84cabc6397b3a
alphalpha <christian@sineswingers.com>
2015-07-28 00:16:48 +0200

    add script to create symlinks

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+files=".antigen.zsh .git-aliases .vimrc .zshrc"
+pathToScript=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+##########
+
+for file in $files; do
+  echo "creating symlink to $file in home directory."
+  ln -fs $pathToScript/$file ~/$file
+done
