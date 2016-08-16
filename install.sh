af5f89caf84c73857d49a62b6e1a2761f7d20bf4
alphalpha <christian@sineswingers.com>
2016-08-16 21:25:42 +0200

    remove . prefix

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-files=".antigen.zsh .git-aliases .octaverc .tm_properties .vimrc .zshrc .zsh-aliases .ctags"
+files="antigen.zsh git-aliases octaverc tm_properties vimrc zshrc zsh-aliases ctags"
 pathToScript=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 ##########
 
 for file in $files; do
   echo "creating symlink to $file in home directory."
-  ln -fs $pathToScript/$file ~/$file
+  ln -fs $pathToScript/$file ~/.$file
 done
