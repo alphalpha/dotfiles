fc06958797fa25c052f3094af10e8bf1dfe5e0f9
alphalpha <christian@sineswingers.com>
2016-08-16 21:45:31 +0200

    move files to subdirectory

--- install.sh
+++ install.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-files="antigen.zsh git-aliases octaverc tm_properties vimrc zshrc zsh-aliases ctags"
 pathToScript=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 ##########
 
-for file in $files; do
-  echo "creating symlink to $file in home directory."
-  ln -fs $pathToScript/$file ~/.$file
+for filename in src/*; do
+  echo "creating symlink to ~/.${filename##*/} in home directory."
+  ln -fs $pathToScript/$filename ~/.${filename##*/}
 done
